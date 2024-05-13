5e9bf013b9b51832ea38688d15c571e353249075
Henok <henokmicrometre@gmail.com>
2024-05-13 20:04:34 +0100

    scafoding redis

---
+++ routes/video.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+router.get('/', function(req, res, next) {
+  res.sendFile('alprVideo.mp4', { root: 'public/uploads'});
+  });
+
+module.exports = router;
